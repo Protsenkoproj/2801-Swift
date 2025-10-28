4adf60b1abd3014d089f7892a33e109bb18b5ca3
SBabchukTM <babchuk.serhii@trident-media.agency>
2025-10-28 15:29:20 +0200

    init

---
+++ RoadRooster/App/RoadRoosterApp.swift
@@ -0,0 +1,17 @@
+//
+//  RoadRoosterApp.swift
+//  RoadRooster
+//
+//  Created by Serhii Babchuk on 28.10.2025.
+//
+
+import SwiftUI
+
+@main
+struct RoadRoosterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            RRLaunchView()
+        }
+    }
+}
